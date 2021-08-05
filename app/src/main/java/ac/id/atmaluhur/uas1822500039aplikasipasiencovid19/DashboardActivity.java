dd2f3c64a1fe929eed202f8d82fb8d0cc38615d6
denny <1822500007@mahasiswa.atmaluhur.ac.id>
2021-08-06 04:32:37 +0700

    upload project

---
+++ app/src/main/java/ac/id/atmaluhur/uas1822500039aplikasipasiencovid19/DashboardActivity.java
@@ -0,0 +1,24 @@
+package ac.id.atmaluhur.uas1822500039aplikasipasiencovid19;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_dashboard );
+    }
+    public void tambah(View View) {
+            Intent i = new Intent(this, TambahdataActivity.class);
+            startActivity(i);
+        }
+    public void tampil(View View) {
+        Intent i = new Intent(this, TampildataActivity.class);
+        startActivity(i);
+    }
+}
